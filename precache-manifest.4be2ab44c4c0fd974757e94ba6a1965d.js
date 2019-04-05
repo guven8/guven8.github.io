db3600f8988b56348670c6ca46e2bfb7d285df1c
Guven <guvenkemal86@gmail.com>
2019-04-05 20:35:25 +0100

    Updates

---
+++ precache-manifest.4be2ab44c4c0fd974757e94ba6a1965d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "52f0de5644578aa8eda9",
+    "url": "/static/js/main.046def5b.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "52f0de5644578aa8eda9",
+    "url": "/static/css/main.96ea5b22.chunk.css"
+  },
+  {
+    "revision": "2a8774ba2756a47e63729fde6facbe0b",
+    "url": "/index.html"
+  }
+];
