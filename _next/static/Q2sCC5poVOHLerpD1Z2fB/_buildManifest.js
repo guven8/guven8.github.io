f296f64d83cd14188392a60570e5fdcf9235afc6
guven8 <guvenkemal86@gmail.com>
2024-02-24 11:29:48 +0000

    Updates

---
+++ _next/static/Q2sCC5poVOHLerpD1Z2fB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/f8f6a203dee584f0.css","static/chunks/pages/index-a51536dc89650660.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
